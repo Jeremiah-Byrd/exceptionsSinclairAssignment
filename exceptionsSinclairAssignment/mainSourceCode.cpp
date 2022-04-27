19ad01ffd14364eced52d99cacb2329c01b2a605
Jeremiah-Byrd <101752111+Jeremiah-Byrd@users.noreply.github.com>
2022-04-26 21:22:47 -0400

    Throwned an exception if a character is not a letter

--- exceptionsSinclairAssignment/mainSourceCode.cpp
+++ exceptionsSinclairAssignment/mainSourceCode.cpp
@@ -20,7 +20,7 @@ int main() {
 	cout << "\n Test #3(Should return Y): " << character('Z', 1);
 	cout << "\n Test #4(should return invalid character exception): " << character('?', 5);
 	cout << "\n Test #5(Should return a transition exception): " << character('A', 32);
-	
+
 }
 
 /**************************************
@@ -29,17 +29,21 @@ throwing exceptions in error conditions.
 **************************************/
 char character(char start, int offset) {
 
-	//If the variables are capitals
-	if (isalpha(start) || isalpha(char(start + offset))) {
-		//ERROR
-	}
-	else if (start >= 65 && start <= 90) {
+//65-90 or 97-122
+	try
+	{
+		//Checking if the character is not a letter
+		if (!(isalpha(start)) || !(isalpha(start + offset))) {
+        
+			//Throwing an invalid char exception
+			throw "invalidCharacterException";
+		}
+		
+		//Checking that a trnasition o
+		
+		
+			
 
 	}
-	else if (start < 65) {
-		//ERROR
-	}else if(start <= 97)
-	return 'a';
 
-	//65-90 or 97-122
 }

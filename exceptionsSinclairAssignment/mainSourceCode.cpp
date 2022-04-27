22aafba9c2b908d595b0cb670600298f95e52783
Jeremiah-Byrd <101752111+Jeremiah-Byrd@users.noreply.github.com>
2022-04-26 21:40:41 -0400

    Finished the function character(Besides returning the proper conversion), and am trying to make my try/catch in the main.

--- exceptionsSinclairAssignment/mainSourceCode.cpp
+++ exceptionsSinclairAssignment/mainSourceCode.cpp
@@ -15,12 +15,16 @@ char character(char start, int offset);
 
 int main() {
 	//Driver 
+	try{
 	cout << "\n Test #1(Should return b): " << character('a', 1);
 	cout << "\n Test #2(Should return invalid range exception): " << character('a', -1);
 	cout << "\n Test #3(Should return Y): " << character('Z', 1);
 	cout << "\n Test #4(should return invalid character exception): " << character('?', 5);
 	cout << "\n Test #5(Should return a transition exception): " << character('A', 32);
-
+	}
+	catch (const char* exception) {
+		cout << exception;
+	}
 }
 
 /**************************************
@@ -29,21 +33,28 @@ throwing exceptions in error conditions.
 **************************************/
 char character(char start, int offset) {
 
-//65-90 or 97-122
-	try
-	{
 		//Checking if the character is not a letter
-		if (!(isalpha(start)) || !(isalpha(start + offset))) {
+		if (!(isalpha(start))) {
         
 			//Throwing an invalid char exception
 			throw "invalidCharacterException";
 		}
+		else if (!(isalpha(start + offset))) {
+			//Throw an invalid range exception
+			throw "invalidRangeException";
+		}
 		
-		//Checking that a trnasition o
-		
-		
-			
+		//Checking that no transition lowercase letters happened 
+		if (islower(start)) {
+			if (char(start+offset) == tolower(start+offset)) {
+				throw "invalidConversionException";
+			}
+		}//Checking that no transitions of capital letters happened
+		else if (isupper(start)) {
+			if (char(start + offset) == toupper(start + offset)) {
+				throw "invalidConversionException";
+			}
+		}
 
-	}
 
 }

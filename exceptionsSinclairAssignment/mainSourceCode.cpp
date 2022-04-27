2b9ec3870a4f9af58bbf653ad49e9c960377c5ed
Jeremiah-Byrd <101752111+Jeremiah-Byrd@users.noreply.github.com>
2022-04-26 20:23:33 -0400

    Added the character function and definition

---
+++ exceptionsSinclairAssignment/mainSourceCode.cpp
@@ -0,0 +1,24 @@
+// Jeremiah Byrd 
+// CIS 1202 101
+// April 26, 2022
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+using namespace std;
+
+//Prototypes 
+char character(char start, int offset);
+
+int main() {
+
+}
+
+/**************************************
+to calculate character offsets,
+throwing exceptions in error conditions.
+**************************************/
+char character(char start, int offset) {
+
+}

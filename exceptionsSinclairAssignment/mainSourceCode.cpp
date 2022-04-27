eefddeaddc608c603bf87aab7d0387e165a5d25c
Jeremiah-Byrd <101752111+Jeremiah-Byrd@users.noreply.github.com>
2022-04-26 21:06:37 -0400

    Made Drivers, Although I still need to fix the character function

--- exceptionsSinclairAssignment/mainSourceCode.cpp
+++ exceptionsSinclairAssignment/mainSourceCode.cpp
@@ -5,6 +5,8 @@
 #include<iostream>
 #include<iomanip>
 #include<cmath>
+#include<cstdlib>
+#include<ctype.h>
 
 using namespace std;
 
@@ -12,7 +14,13 @@ using namespace std;
 char character(char start, int offset);
 
 int main() {
-
+	//Driver 
+	cout << "\n Test #1(Should return b): " << character('a', 1);
+	cout << "\n Test #2(Should return invalid range exception): " << character('a', -1);
+	cout << "\n Test #3(Should return Y): " << character('Z', 1);
+	cout << "\n Test #4(should return invalid character exception): " << character('?', 5);
+	cout << "\n Test #5(Should return a transition exception): " << character('A', 32);
+	
 }
 
 /**************************************
@@ -21,4 +29,17 @@ throwing exceptions in error conditions.
 **************************************/
 char character(char start, int offset) {
 
+	//If the variables are capitals
+	if (isalpha(start) || isalpha(char(start + offset))) {
+		//ERROR
+	}
+	else if (start >= 65 && start <= 90) {
+
+	}
+	else if (start < 65) {
+		//ERROR
+	}else if(start <= 97)
+	return 'a';
+
+	//65-90 or 97-122
 }
